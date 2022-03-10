cd5f995ccb747fba6801468c507e462cbbf1045b
nhungnguyenhuu <nhung.nguyen.hmu@gmail.com>
2022-03-10 22:57:52 +0700

    upload

---
+++ manage-customer/database/seeds/CustomerTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CustomerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Illuminate\Support\Facades\DB::table();
+
+    }
+}
